bc043b50980f4bafd8b11a257ba65f2ee2d3a35b
psyomn <lethaljellybean@gmail.com>
2013-03-26 18:25:22 -0400

    UI is done. Or 90% done at least.

--- src/com/project/presentation/MainGUI.java
+++ src/com/project/presentation/MainGUI.java
@@ -6,8 +6,11 @@ import java.awt.GridBagLayout;
 import java.util.ArrayList;
 
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
-import javax.swing.JList;
+import javax.swing.JLabel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 
 /**
  * This is the main view to the actual program.
@@ -20,8 +23,16 @@ public class MainGUI extends JFrame
   protected JButton mButtonParseChallenge = new JButton("Parse Challenge");
   protected JButton mButtonDoIt = new JButton("Do it...");
   protected JButton mButtonParseServices = new JButton("Parse Services");
+  protected JButton mButtonStop = new JButton("Stop");
+  protected JTextArea mTextOutput = new JTextArea(16,58);
+  protected JScrollPane mTextScroll = new JScrollPane(mTextOutput);
   /* From 1 -> 10 */
-  protected JList mListBranching = new JList();
+  protected JLabel mListLabel = new JLabel("Parallelism");
+  protected JComboBox<String> mListBranching = null;
+  
+  protected JLabel mSolutionLabel = new JLabel("Num of solutions:");
+  protected JComboBox<String> mSolutionCombo = null;
+  
   protected GridBagLayout mLayout = new GridBagLayout();
   protected GridBagConstraints mGBConstraints = new GridBagConstraints();
   
@@ -29,19 +40,23 @@ public class MainGUI extends JFrame
   public MainGUI()
   {
     super("WSC 2009 | Unamed Team");
-  
+    initComponents();
     ArrayList<Component> wArr = new ArrayList<Component>();
-    
+        
     /* 
      * Must add the components here in the order you want them shown in the
      * actual ui
      */
+    wArr.add(mTextScroll);
     wArr.add(mButtonParseChallenge);
     wArr.add(mButtonParseServices);
     wArr.add(mButtonParseTaxonomy);
+    wArr.add(mListLabel);
+    wArr.add(mListBranching);
+    wArr.add(mSolutionLabel);
+    wArr.add(mSolutionCombo);
     wArr.add(mButtonDoIt);
-    wArr.add(mListBranching);
-    wArr.add(mListBranching);
+    wArr.add(mButtonStop);
     
     mGBConstraints.gridx = 0;
     mGBConstraints.gridy = 0;
@@ -51,11 +66,41 @@ public class MainGUI extends JFrame
     
     for (int i = 0; i < wArr.size(); ++i)
     {
+      if (wArr.get(i) == mTextScroll)
+        mGBConstraints.weighty = 2.0f;
+      
       this.add(wArr.get(i), mGBConstraints);
       ++mGBConstraints.gridy;
+      
+      if (wArr.get(i) == mTextScroll)
+        mGBConstraints.weighty = 0.0f;
     }
     
-    this.setBounds(10, 10, 500, 400);
-    this.setResizable(false);
+    this.setBounds(10, 10, 700, 600);
+    this.setResizable(true);
   }
+
+  private void initComponents()
+  {
+    initListBranchingComponent();
+    initSolutionComboComponent();
+  }
+  
+  /**
+   * Add the items for the list for branching
+   */
+  private void initListBranchingComponent()
+  {
+    String[] items = new String[10];
+    for(int i = 0; i < 10; items[i] = "" + (++i));
+    mListBranching = new JComboBox<String>(items);
+  }
+  
+  private void initSolutionComboComponent()
+  {
+    String[] items = new String[10];
+    for(int i = 0; i < 10; items[i] = "" + (++i));
+    mSolutionCombo = new JComboBox<String>(items);
+  }
+  
 }
